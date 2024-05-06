5df3a1afdabf64fb3c8ce61a1c801a2addf9a886
Val <saviorand@gmail.com>
2024-05-06 16:43:05 +0200

    add server and preview commands

--- extension.js
+++ extension.js
@@ -1,27 +1,66 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
 const path = require('path');
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
+const { exec } = require('child_process');
 
 /**
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "praxis-ide-vscode-community" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
-	// The commandId parameter must match the command field in package.json
 	let disposable = vscode.commands.registerCommand('praxis-ide-vscode-community.parseFile', function () {
 		readCurrentFile();
 	});
 
 	context.subscriptions.push(disposable);
+
+	let disposable2 = vscode.commands.registerCommand('praxis-ide-vscode-community.openPreview', function () {
+		let livePreview = vscode.extensions.getExtension('ms-vscode.live-server')
+		if (livePreview) {
+			vscode.commands.executeCommand('livePreview.start.preview.atFileString', 'node_modules/praxis-ide-saviorand/index.html');
+		} else {
+			vscode.window.showInformationMessage('Live Preview extension not found!');
+		}
+	});
+
+	context.subscriptions.push(disposable2);
+
+	let disposableRun = vscode.commands.registerCommand('praxis-ide-vscode-community.startPreviewServer', function () {
+        const workspaceFolders = vscode.workspace.workspaceFolders;
+        if (!workspaceFolders) {
+            vscode.window.showErrorMessage('No workspace is opened.');
+            return;
+        }
+
+        const workspaceRoot = workspaceFolders[0].uri.fsPath; // Get the root of the first workspace folder
+        const nodeCommand = 'node';
+        const scriptPath = path.join(workspaceRoot, 'node_modules/praxis-ide-saviorand/app.js');
+        const command = `${nodeCommand} "${scriptPath}"`;
+
+        serverProcess = exec(command, (error, stdout, stderr) => {
+            if (error) {
+                vscode.window.showErrorMessage(`Failed to run script: ${error.message}`);
+                return;
+            }
+            if (stderr) {
+                vscode.window.showErrorMessage(`Script execution error: ${stderr}`);
+                return;
+            }
+            vscode.window.showInformationMessage(`Script output: ${stdout}`);
+        });
+
+        vscode.window.showInformationMessage('Server started.');
+    });
+
+    let disposableStop = vscode.commands.registerCommand('praxis-ide-vscode-community.stopPreviewServer', function () {
+        if (serverProcess) {
+            serverProcess.kill();
+            vscode.window.showInformationMessage('Server stopped.');
+        } else {
+            vscode.window.showErrorMessage('Server is not running.');
+        }
+    });
+
+    context.subscriptions.push(disposableRun, disposableStop);
 
 	// Registering a listener for text document changes
     let changeDisposable = vscode.workspace.onDidChangeTextDocument((event) => {
@@ -33,47 +72,43 @@ function activate(context) {
     context.subscriptions.push(changeDisposable);
 }
 
-
 function readCurrentFile() {
     const editor = vscode.window.activeTextEditor;
     if (editor) {
         const document = editor.document;
         const content = document.getText();
 
-        if (vscode.workspace.workspaceFolders) {
-            const workspaceFolder = vscode.workspace.workspaceFolders[0];
-            const filePath = vscode.Uri.file(path.join(workspaceFolder.uri.fsPath, 'tmp.pl'));
+        // Check if the file has a .pl extension
+        const fileExtension = path.extname(document.fileName);
+        if (fileExtension === '.pl') {
+            if (vscode.workspace.workspaceFolders) {
+                const workspaceFolder = vscode.workspace.workspaceFolders[0];
+                const filePath = vscode.Uri.file(path.join(workspaceFolder.uri.fsPath, '.vscode/praxis/tmp.pl'));
 
-            const writeData = Buffer.from(content, 'utf8');
+                const writeData = Buffer.from(content, 'utf8');
 
-            vscode.workspace.fs.writeFile(filePath, writeData).then(() => {
-                console.log('File successfully written to:', filePath.fsPath);
-            }, (error) => {
-                console.error('Failed to write file:', error);
-            });
+                vscode.workspace.fs.writeFile(filePath, writeData).then(() => {
+                    console.log('File successfully written to:', filePath.fsPath);
+                }, (error) => {
+                    console.error('Failed to write file:', error);
+                });
+            } else {
+                vscode.window.showInformationMessage('No workspace folder found!');
+            }
         } else {
-            vscode.window.showInformationMessage('No workspace folder found!');
+            console.log('The open file does not have a .pl extension, not writing to tmp.pl');
         }
     } else {
         vscode.window.showInformationMessage('No active editor!');
     }
 }
-// function readCurrentFile() {
-//     const editor = vscode.window.activeTextEditor;
-//     if (editor) {
-//         const document = editor.document;
-//         const content = document.getText();
-//         // console.log(text);  // Log the content to the debug console
-// 		const filePath = path.join(vscode.workspace.workspaceFolders[0].uri, 'tmp.pl');
-// 		fs.writeFileSync(filePath, content, 'utf8');
-// 		console.log('File written to: ' + filePath);
-//     } else {
-//         vscode.window.showInformationMessage('No active editor!');
-//     }
-// }
 
 // This method is called when your extension is deactivated
-function deactivate() {}
+function deactivate() {
+	if (serverProcess) {
+        serverProcess.kill();
+    }
+}
 
 module.exports = {
 	activate,
